a286190ccaa6f5d4024c67e845ad0312d8392f64
SHOAIB KHAN <15shoaibkhan@gmail.com>
2024-12-21 21:03:13 +0530

    homepage3

--- frontend/src/components/Home/home.js
+++ frontend/src/components/Home/home.js
@@ -4,10 +4,12 @@ import Intro from './intro';
 import Initiatives from './initiatives';
 import Banner from './banner';
 import Launchpad from './launchpad';
+import Hero from './hero';
 
 function Home() {
   return (
     <div>
+    <Hero/>
     <Intro/>
     <Startup/>
     <Initiatives/>
