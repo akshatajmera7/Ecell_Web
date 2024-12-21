f65dc8b16c8bff63b44b46f5a2de7772a2f3a71d
SHOAIB KHAN <15shoaibkhan@gmail.com>
2024-12-21 22:53:46 +0530

    changes

--- frontend/src/components/Home/hero.js
+++ frontend/src/components/Home/hero.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import videohp from '../../assets/videohp.mp4';
-import '././Hero.css';
+import './Hero.css';
 
-const Hero = () =>{
+const Hero = () => {
     return (
         <div className='main'>
             <video src={videohp} autoPlay loop muted />
             <div className="content">
-                <h1 className='vidint'>E-Cell</h1>
+                <h1 className='vidint'>E-Cell BITS Pilani Hyderabad Campus</h1>
+                <p className="subheading">Empowering Entrepreneurs, Inspiring Innovation</p>
             </div>
         </div>
-    )
+    );
 }
 
-export default Hero
+export default Hero;
