f54670c886738a09560ac417e78d7915863b38f3
CodingQueen-gg <niagupta84@gmail.com>
2024-12-22 11:48:40 +0530

    responsive

--- frontend/src/components/Home/hero.js
+++ frontend/src/components/Home/hero.js
@@ -4,8 +4,12 @@ import './Hero.css';
 
 const Hero = () => {
     return (
-        <div className='main'>
-            <video src={videohp} autoPlay loop muted />
+        <div className="main">
+            <div className="video-container">
+                <video autoPlay muted loop playsInline>
+                    <source src={videohp} type="video/mp4" />
+                </video>
+            </div>
             <div className="content">
                 <h2 className='vidint'>E-Cell BITS Pilani Hyderabad Campus</h2>
                 <p className="subheading">Empowering Entrepreneurs, Inspiring Innovation</p>
