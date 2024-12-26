cf39832f850c2e7152396c3a79c565a75a35741e
Vrinda-26 <saboo.vrinda260206@gmail.com>
2024-12-26 18:45:25 +0530

    Programs

--- frontend/src/components/Programs/memory.js
+++ frontend/src/components/Programs/memory.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import img1 from '../../assets/study.jpg';
-import img2 from '../../assets/pitch.jpg';
-import img3 from '../../assets/group.jpg';
+import img1 from '../../assets/LaunchPad.JPG';
+import img2 from '../../assets/Speaker_session.png';
+import img3 from '../../assets/startup_expo.JPG';
 import img4 from '../../assets/auction.jpg';
 import img5 from '../../assets/class.jpg';
-import img6 from '../../assets/lec.jpg';
+import img6 from '../../assets/pitch.jpg';
 
 const Memories = () => {
   const [activeIndex, setActiveIndex] = useState(0);
