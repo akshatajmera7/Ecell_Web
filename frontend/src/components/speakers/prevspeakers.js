29e4c107e0fcf0708907c56f3bb59e7e3ae73e12
ArnavG5107 <arnavgupta.svt@gmail.com>
2025-02-10 01:42:59 +0530

    Update prevspeakers.js

--- frontend/src/components/speakers/prevspeakers.js
+++ frontend/src/components/speakers/prevspeakers.js
@@ -1,55 +1,119 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import img1 from '../../assets/SPEAKER PICS/vp_india_naidu.jpg'; 
+import img2 from '../../assets/SPEAKER PICS/piyush-goyal.jpeg'; 
+import img3 from '../../assets/SPEAKER PICS/kiran_mazumdar_shaw.jpeg'; 
+import img4 from '../../assets/SPEAKER PICS/vinod-dham.jpeg'; 
+import img5 from '../../assets/SPEAKER PICS/Nilesh-Kothari1.png'; 
+import img6 from '../../assets/SPEAKER PICS/kshitij_khandelwal.jpeg'; 
+import img7 from '../../assets/SPEAKER PICS/ANAND-SRINIVASAN.webp'; 
+import img8 from '../../assets/SPEAKER PICS/sam.jpg';
+import img9 from '../../assets/SPEAKER PICS/TARUN-KATHIYAL.webp';
+import img10 from '../../assets/SPEAKER PICS/A_SIVANTHANU.png';
+import img11 from '../../assets/SPEAKER PICS/ANKUSH-SINGHLA.jpeg';
+import img12 from '../../assets/SPEAKER PICS/KAVITHA.jpeg';
+import img13 from '../../assets/SPEAKER PICS/greg.webp'; 
+import img14 from '../../assets/SPEAKER PICS/pranali.png';
+import img15 from '../../assets/SPEAKER PICS/varun.jpeg';
+import img16 from '../../assets/SPEAKER PICS/PRANAY-PAI.png';
 
 const PreviousSpeakers = () => {
   const speakers = [
     {
       id: 1,
-      name: "Sonam Wangchuck",
-      role: "Innovator, Engineer and Education Reformist",
-      image: "/api/placeholder/200/200" // Replace with your image URLs
+      name: "SHRI VENKAIAH NAIDU",
+      role: "FORMER VICE-PRESIDENT OF INDIA",
+      image: img1 // Replace with your image URLs
     },
     {
       id: 2,
-      name: "Vishal Gondal",
-      role: "Founder & CEO, GOQii",
-      image: "/api/placeholder/200/200"
+      name: "PIYUSH GOYAL",
+      role: "UNION CABINET MINISTER",
+      image: img2
     },
     {
       id: 3,
-      name: "Vipul Joshi",
-      role: "Co-founder and CFO, IdeaForge",
-      image: "/api/placeholder/200/200"
+      name: "KIRAN MAZUMDAR SHAW",
+      role: "CHAIRPERSON BIOCON",
+      image: img3
     },
     {
       id: 4,
-      name: "Aviral Bhatnagar",
-      role: "Founder and Managing Partner, A Junior VC",
-      image: "/api/placeholder/200/200"
+      name: "VINOD DHAM",
+      role: "INDIAN INVENTOR",
+      image:img4
     },
     {
       id: 5,
-      name: "Ritu Verma",
-      role: "Co-Founder and Managing Partner, Ankur Capital",
-      image: "/api/placeholder/200/200"
+      name: "NILESH KOTHARI",
+      role: "CO-FOUNDER TRIFECTA CAPITAL",
+      image: img5
     },
     {
       id: 6,
-      name: "Nitin Jain",
-      role: "Co-founder & CBO, OfBusiness",
-      image: "/api/placeholder/200/200"
+      name: "KSHTIJ KHANDELWAL",
+      role: "CTO - PIXXEL",
+      image: img6
     },
     {
       id: 7,
-      name: "Gaurav Chaudhary",
-      role: "Content Creator, Technical guruji",
-      image: "/api/placeholder/200/200"
+      name: "ANAND SRINIVASAN",
+      role: "CO FOUNDER AKASA AIR",
+      image: img7
     },
     {
       id: 8,
-      name: "Viraj Bahl",
-      role: "Shark at SharkTank India and Co-Founder, VEEBA",
-      image: "/api/placeholder/200/200"
+      name: "SAM PITRODA",
+      role: "INDIAN INVENTOR ",
+      image: img8
+    },
+    {
+      id: 9,
+      name: "TARUN KATIYAL",
+      role: "FOUNDER COTO EX-CEO ZEE5",
+      image:img9
+    },
+    {
+      id: 10,
+      name: "DR.A SIVANTHANU PILLAI",
+      role: "INDIAN SCIENTIST DRDO AND ISRO",
+      image:img10
+    },
+    {
+      id: 11,
+      name: "ANKUSH SINGLA",
+      role: "CO-FOUNDER CODING NINJAS",
+      image: img11
+    },
+    {
+      id: 12,
+      name: "KAVITHA SUBRAMANIAN",
+      role: "CO FOUNDER UPSTOX",
+      image: img12
+    },
+    {
+      id: 13,
+      name: "GREG MOORAN",
+      role: "CEO ZOOMCAR",
+      image: img13
+    },
+    {
+      id: 14,
+      name: "PRANALI MEHTA",
+      role: "VENTURES AND ACCELERATION ANTHILL",
+      image:img14
+    },
+    {
+      id: 15,
+      name: "VARUN SRIDHAR",
+      role: "CEO PAYTM COMPANY",
+      image: img15
+    },
+    {
+      id: 16,
+      name: "PRANAY PAI",
+      role: "FOUNDING PARTNER 30NE4 CAPITAL",
+      image: img16
     }
   ];
 
