d18a505f0f540a29d4bd5998da9fcb0ba0dc27e7
ArnavG5107 <arnavgupta.svt@gmail.com>
2025-02-12 23:04:41 +0530

    Update list.js

--- frontend/src/components/events/list.js
+++ frontend/src/components/events/list.js
@@ -51,7 +51,7 @@ const EventCard = ({ event, index }) => {
                 Register Now
               </a>
               <a
-                href={event.registerLink} // Update with the explore link if you have a specific one
+                href={event.exploreLink} // Update with the explore link if you have a specific one
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-1/2 text-center py-3 px-6 rounded-lg bg-blue-900 text-white font-semibold transform transition-all duration-300 hover:scale-105 hover:shadow-lg hover:shadow-blue-600/50 active:scale-95"
@@ -68,13 +68,13 @@ const EventCard = ({ event, index }) => {
 
 const EventsPage = () => {
   const events = [
-    { id: 1, title: "Startup Expo", description: "Unleash innovation and connect with industry leaders at the ultimate Startup Expo!", images: [img1], registerLink: "/launchpad/startup_expo"},
-    { id: 2, title: "Pitch Perfect", description: "Where innovation meets opportunity, and entrepreneurs and investors thrive together.", images: [img2], registerLink: "/launchpad/pitch_perfect"},
-    { id: 3, title: "Internship Drive", description: "Launch your career with the Internship Drive – connect, learn, and grow with top startups!", images: [img3], registerLink: "/launchpad/internship_drive" },
-    { id: 4, title: "Beyond Profits", description: "Empowering socially-conscious entrepreneurs to create change and unlock incredible opportunities.", images: [img4], registerLink: "/launchpad/beyond_profits" },
-    { id: 5, title: "Teen Tycoons", description: "Shaping the next generation of entrepreneurs through dynamic challenges and expert mentorship.", images: [img5], registerLink: "/launchpad/teen_tycoons" },
-    { id: 6, title: "Pitcher's Pilot", description: "A dynamic platform for entrepreneurs to showcase their ventures, gain valuable feedback, and connect with investors for growth and success", images: [img6], registerLink: "/launchpad/pitchers_pilot" },
-    { id: 7, title: "Ground Reality", description: "Pitch your startup to top investors and win Rs. 2 Lakhs with mentorship and resources for growth!", images: [img7], registerLink: "/launchpad/ground_reality" }
+    { id: 1, title: "Startup Expo", description: "Unleash innovation and connect with industry leaders at the ultimate Startup Expo!", images: [img1],  registerLink: "https://unstop.com/conferences/startup-expo-launchpad-25-birla-institute-of-technology-science-pilani-hyderabad-campus-1317940",exploreLink:"/launchpad/startup_expo"},
+    { id: 2, title: "Pitch Perfect", description: "Where innovation meets opportunity, and entrepreneurs and investors thrive together.", images: [img2],registerLink: "https://unstop.com/competitions/pitch-perfect-an-online-bussiness-plan-competition-launchpad-25-birla-institute-of-technology-and-sciences--1319979",exploreLink:"/launchpad/pitch_perfect"},
+    { id: 3, title: "Internship Drive", description: "Launch your career with the Internship Drive – connect, learn, and grow with top startups!", images: [img3],registerLink: "https://bharatversity.com/event/67878d18c9499900218d641f",exploreLink:"/launchpad/internship_drive"},
+    { id: 4, title: "Beyond Profits", description: "Empowering socially-conscious entrepreneurs to create change and unlock incredible opportunities.", images: [img4],registerLink: "https://unstop.com/competitions/beyond-profits-launchpad-25-bits-1361695",exploreLink:"/launchpad/beyond_profits"},
+    { id: 5, title: "Teen Tycoons", description: "Shaping the next generation of entrepreneurs through dynamic challenges and expert mentorship.", images: [img5], registerLink: "https://bharatversity.com/event/679be5698f8b890021015fd0" ,exploreLink:"/launchpad/teen_tycoons"},
+    { id: 6, title: "Pitcher's Pilot", description: "A dynamic platform for entrepreneurs to showcase their ventures, gain valuable feedback, and connect with investors for growth and success", images: [img6],  registerLink: "https://unstop.com/o/zN4GnQ6?lb=jL0ByYM&utm_medium=Share&utm_source=shortUrl",exploreLink:"/launchpad/pitchers_pilot" },
+    { id: 7, title: "Ground Reality", description: "Pitch your startup to top investors and win Rs. 2 Lakhs with mentorship and resources for growth!", images: [img7],registerLink: "https://unstop.com/competitions/ground-reality-a-business-plan-competition-birla-institute-of-technology-science-pilani-hyderabad-campus-1316405",exploreLink:"/launchpad/ground_reality" }
   ];
 
   return (
@@ -90,4 +90,3 @@ const EventsPage = () => {
 };
 
 export default EventsPage;
-
