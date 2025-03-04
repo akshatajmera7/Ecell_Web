780eb5452484be115ffa2b17168853adc6888e33
Vrinda-26 <saboo.vrinda260206@gmail.com>
2025-03-04 20:03:21 +0530

    registration links

--- frontend/src/components/lpevents/id/hero.js
+++ frontend/src/components/lpevents/id/hero.js
@@ -13,7 +13,7 @@ const ProductExplorer = () => {
         </p>
         <button
   className="px-6 py-3 text-lg font-semibold bg-blue-600 hover:bg-blue-500 text-white rounded-2xl shadow-md transition-transform transform hover:scale-105"
-  onClick={() => { window.location.href = "https://pmny.in/ZJi5eWmeNiNH"; }}
+  onClick={() => { window.location.href = "https://rzp.io/rzp/MFXKL5k0"; }}
 >
   Register Now
 </button>
